25cf40d74990f1b623a36e085d6c3b415d2179d4
Cole Ramos <cole_ramos@hotmail.com>
2019-03-02 23:40:24 -0800

    Adding backend for regist

--- app/src/main/java/com/example/sublease/LoginActivity.java
+++ app/src/main/java/com/example/sublease/LoginActivity.java
@@ -1,4 +1,68 @@
 package com.example.sublease;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignup;
+    private TextView textViewSignin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        textViewSignup = (TextView) findViewById(R.id.textViewSignin);
+
+        buttonRegister.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //email is empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            //stopping funct execution further
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            //password is empty
+            Toast.makeText(this, "Please enter pawword", Toast.LENGTH_SHORT).show();
+            //stopping funct execution further
+            return;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonRegister){
+            registerUser();
+        }
+        if(view == textViewSignin){
+            //will open login activity here
+        }
+    }
 }

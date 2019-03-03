aa41d780131ea8cf1c8d92ad98ec9567c5c8336d
Ben <bendreyer21@gmail.com>
2019-03-03 11:17:31 -0800

    map navbar

--- app/src/main/java/com/example/sublease/HomeFragment.java
+++ app/src/main/java/com/example/sublease/HomeFragment.java
@@ -17,8 +17,8 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //Intent intent = new Intent(getActivity(), MapsActivity.class);
-        //startActivity(intent);
+        Intent intent = new Intent(getActivity(), MapsActivity.class);
+        startActivity(intent);
         return inflater.inflate(R.layout.fragment_home, null);
     }
 }
